c71ef13ceebfb4e0bffcc02247daa1641a38071c
kumaradharma <dharma.idrive@gmail.com>
2020-04-29 12:48:56 +0530

    alignment issues

--- TrueLoveCalculator/TrueLoveCalculator/ViewController.swift
+++ TrueLoveCalculator/TrueLoveCalculator/ViewController.swift
@@ -22,11 +22,11 @@ class ViewController: UIViewController {
 
 	var shortString = [] as Array
 
-override func viewDidLoad() {
-super.viewDidLoad()
-	lovebetween.isHidden = true
-	percentageLabel.isHidden = true
-	girlandboynames.isHidden = true
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		lovebetween.isHidden = true
+		percentageLabel.isHidden = true
+		girlandboynames.isHidden = true
 
 }
 
@@ -78,66 +78,66 @@ super.viewDidLoad()
 
 }
 
-func firstStep(maleName:String, femaleName:String) -> Array<Int>
-{
-let combinedName = "\(maleName) loves \(femaleName)"
+	func firstStep(maleName:String, femaleName:String) -> Array<Int>
+	{
+		let combinedName = "\(maleName) loves \(femaleName)"
 
-let lowerVal = combinedName.lowercased()
-let tempVal = lowerVal.replacingOccurrences(of: " ", with: "")
-let arr = Array(tempVal)
+		let lowerVal = combinedName.lowercased()
+		let tempVal = lowerVal.replacingOccurrences(of: " ", with: "")
+		let arr = Array(tempVal)
 
-var tempArraycount = [Int]()
-var stringArray = [Character]()
-for temp in arr
-{
+		var tempArraycount = [Int]()
+		var stringArray = [Character]()
+		for temp in arr
+		{
 
-	var count:Int = 0
-  if !stringArray.contains(temp)
-  {
-     for temp1 in arr
-      {
-        if temp == temp1
-         {
-         count = count+1
-         }
-      }
-         tempArraycount.append(count)
-           stringArray.append(temp)
-  }
+			var count:Int = 0
+			if !stringArray.contains(temp)
+			{
+				for temp1 in arr
+				{
+					if temp == temp1
+					{
+						count = count+1
+					}
+				}
+				tempArraycount.append(count)
+				stringArray.append(temp)
+			}
 
-}
-return tempArraycount
-}
+		}
+		return tempArraycount
+	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-// Dispose of any resources that can be recreated.
+		// Dispose of any resources that can be recreated.
 	}
 	@IBAction func calculateLove(_ sender: Any)
 	{
 		let text_female: String = femaleName.text!
 		let text_male: String = maleName.text!
 
-			var finalString:String  = ""
-			let FirstStep = (firstStep(maleName:text_male,femaleName:text_female))
-			var output = FirstStep
-			repeat {
-				shortString = [] as Array
-				shortString =  shortenNumber(firstStep: output)
-				if shortString.count==2
-				{
-					lovebetween.isHidden = false
-					percentageLabel.isHidden = false
-					girlandboynames.isHidden = false
-					calculatebutton.setTitle("Reset", for: .normal)
-					girlandboynames.text = ("\(text_female) & \(text_male)")
-					percentageLabel.text = ("\(shortString[0])\(shortString[1])%")
-					break
-				}
-				output = shortString as! [Int]
-			}while(shortString.count>2);
+		var finalString:String  = ""
+		let FirstStep = (firstStep(maleName:text_male,femaleName:text_female))
+		var output = FirstStep
+		repeat {
+			shortString = [] as Array
+			shortString =  shortenNumber(firstStep: output)
+			if shortString.count==2
+			{
+				lovebetween.isHidden = false
+				percentageLabel.isHidden = false
+				girlandboynames.isHidden = false
+				calculatebutton.setTitle("Reset", for: .normal)
+				girlandboynames.text = ("\(text_female) & \(text_male)")
+				percentageLabel.text = ("\(shortString[0])\(shortString[1])%")
+				break
+			}
+			output = shortString as! [Int]
+		}while(shortString.count>2);
 
-	  
+
 
 
 	}
